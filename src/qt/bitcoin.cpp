75ca05f4d8b8fd0d0f1cc94fb653505b555182fd
mdfkbtc <mdfkbtc@gmail.com>
2019-06-29 02:49:34 +0200

    Add Veles copyrights
    (cherry picked from commit 549a50b60fc41c7ea167046f6b6eeb76431ff9cb)
    (cherry picked from commit 1138b279001b93853bcd0f504fc9b529299b2eb5)

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2011-2019 The Bitcoin Core developers
 // Copyright (c) 2014-2017 The Dash Core developers
 // Copyright (c) 2018 FXTC developers
+// Copyright (c) 2018-2019 The Veles Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
