b537c4d7fbb1fcbf8947b0af1bdb6b53b6118945
mdfkbtc <mdfkbtc@gmail.com>
2019-07-04 00:30:04 +0200

    Travis: fix repo name for testing ..
    (cherry picked from commit 5a9cd7a99cee180f2d659a555cb054fada377bda)

--- .travis/lint_06_script.sh
+++ .travis/lint_06_script.sh
@@ -18,7 +18,11 @@ test/lint/check-doc.py
 test/lint/check-rpc-mappings.py .
 test/lint/lint-all.sh
 
+<<<<<<< HEAD
 if [ "$TRAVIS_REPO_SLUG" = "veles/veles" -a "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+=======
+if [ "$TRAVIS_REPO_SLUG" = "mdfkbtc1/veles" -a "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+>>>>>>> 5a9cd7a99... Travis: fix repo name for testing ..
     git log --merges --before="2 days ago" -1 --format='%H' > ./contrib/verify-commits/trusted-sha512-root-commit
     while read -r LINE; do travis_retry gpg --keyserver hkp://subset.pool.sks-keyservers.net --recv-keys $LINE; done < contrib/verify-commits/trusted-keys &&
     travis_wait 50 contrib/verify-commits/verify-commits.py --clean-merge=2;
