09163a39a95e946ca30606b06ae1b7141c945b91
mdfkbtc <mdfkbtc@gmail.com>
2019-07-06 19:49:07 +0200

    add veleslogo.h

---
+++ src/veleslogo.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018-2019 The Veles Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_VELESLOGO_H
+#define BITCOIN_VELESLOGO_H
+
+#include <string>
+
+static const std::string strVelesCoreLogoAscii = "\
+ ____   ____     .__                _________                       \n\
+_\\___\\_/___/____ |  |   ____   _____\\_   ___ \\  ___________   ____  \n\
+\\___________/__ \\|  | _/ __ \\ /  ___/    \\  \\/ /  _ \\_  __ \\_/ __ \\ \n\
+   \\  Y  /\\  ___/|  |_\\  ___/ \\___ \\\\     \\___(  <_> )  | \\/\\  ___/ \n\
+    \\___/  \\___  >____/\\___  >____  >\\______  /\\____/|__|    \\___  >\n\
+               \\/          \\/     \\/        \\/                   \\/ \n";
+
+#endif // BITCOIN_VELESLOGO_H
